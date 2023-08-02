a2e6b709bcf50806f1bd5c917b933b6b781655bd
vellimole0621 <vellimole0621@gmail.com>
2023-08-03 00:48:02 +0900

    function

---
+++ PHP/syntax/function.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+
+function 関数の定義
+
+基本文法
+
+인수가 없는 경우
+function 関数名()
+{
+    // 処理を記入
+}
+
+인수가 있는 경우
+function 関数名(引数1, 引数2, ...)
+{
+    // 処理を記入
+}
+```
+
+예
+
+function calculateBmi($height, $weight) {
+    // BMI = 体重[kg] / (身長[m] * 身長[m])
+    return $weight / ($height ** 2);
+}
+
+echo calculateBmi(1.65, 58); // 「21.303948576676」を出力
+
+*/
+
+?>
