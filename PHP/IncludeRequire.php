2f7f65792ff94f41203268a3882fb73a0fdc44ab
vellimole0621 <vellimole0621@gmail.com>
2023-08-02 23:02:18 +0900

    include require

---
+++ PHP/IncludeRequire.php
@@ -0,0 +1,21 @@
+<?php
+
+include __DIR__."/PHP/IncludeRequire.php";
+
+
+/* include & require 
+
+ 공통점 : 다른 파일에 해당 php 파일의 내용을 사용한다는 점에서 동일
+
+차이점 :
+
+- requrie
+ - 해당 파일이 없는 경우, require 부분에서 실행되지 않고 에러가 출력됨
+- include
+ - 해당 파일이 없는 경우, include 부분 뒤는 실행된다. 에러는 출력되나 이어서 실행됨.
+
+ */
+
+?>
+
+
