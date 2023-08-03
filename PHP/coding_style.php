6f36ec26d612f20dbf63ddff8b6d3b416a2bdc06
vellimole0621 <vellimole0621@gmail.com>
2023-08-03 12:01:14 +0900

    coding_style

---
+++ PHP/coding_style.php
@@ -0,0 +1,11 @@
+<!-- 
+Reference
+https://github.com/php-fig/fig-standards/blob/master/accepted/PSR-1-basic-coding-standard.md
+https://github.com/php-fig/fig-standards/blob/master/accepted/PSR-12-extended-coding-style-guide.md 
+-->
+
+
+<?php
+
+
+?>
