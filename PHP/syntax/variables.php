8f2d24967820eca703908cae0c854d3a289080ca
vellimole0621 <vellimole0621@gmail.com>
2023-08-03 12:01:54 +0900

    variables

---
+++ PHP/syntax/variables.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+variables 変数
+
+PHP 변수는 변수명 앞에 $를 붙인다.
+
+boolean(真偽値)
+
+$isCorrectAnswer = true;
+$isCorrectAnswer = false;
+
+
+int(数値型)
+
+$questionNum = 1;
+
+
+string(文字列型)
+
+$answer = 'A';
+$explanation = 'Aはなんとかかんとかです。ちなみにexplanationとは解説という意味です。';
+
+*/
+
+?>
