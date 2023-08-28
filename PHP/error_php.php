044772d66f35239542f39e4989d623d8f8a6da02
vellimole0621 <vellimole0621@gmail.com>
2023-08-28 23:14:36 +0900

    error php

---
+++ PHP/error_php.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+에러
+Parse Error : Syntax error
+Fatal Error : 발생되면 처리가 멈추는 에러
+Warning : 발생되어도 처리가 정지되지 않는다. 문제가 발생된 곳 이후에 의도하지않은 결과가 발생할 가능성이 있다.
+
+php.ini 란
+=> php의 설정을 포함하고 있는 파일
+
+php.ini 파일의 경로 찾는 방법
+=> php -i | grep php.ini
+
+php.ini 파일에 대해
+;를 행의 가장 앞에 붙이면 주석처리된다.
+
+display_errors = On 을 유지하도록 하자.
+*/
