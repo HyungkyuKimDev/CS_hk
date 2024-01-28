9acd891af1f679ce59f48977f9fb893b373d0d07
vellimole0621 <vellimole0621@gmail.com>
2024-01-28 15:24:58 +0900

    FIX Add Comment

--- JAVA/Algorithm/AtmAlgorithm.java
+++ JAVA/Algorithm/AtmAlgorithm.java
@@ -7,12 +7,16 @@ public class AtmAlgorithm{
 		Scanner sc = new Scanner(System.in);
 		
 		// 주어진 값 확인
+		// 횟수
 		int num = sc.nextInt();
+
+		// 각 기다리는 값
 		int[] input = new int [num];
 		for(int k = 0; k < num; k++){
 			input[k] = sc.nextInt();
 		}
 
+		// 버블 정렬을 통해 최솟값이 되도록 정렬
 		for(int i = input.length - 1; i > 0; i--) {
 			for(int j = 0; j < i; j++) {
 				if(input[j] > input[j + 1]) {
@@ -23,8 +27,10 @@ public class AtmAlgorithm{
 			}
 		}
 		
+		// 기다리는 시간 합 저장 변수 선언
 		int sum = 0;
 
+		// 부분합으로 전체 시간 합 구ㅁ
 		for(int t = 0; t < input.length; t++)
 		{	
 			if ( t == 0){
@@ -37,6 +43,7 @@ public class AtmAlgorithm{
 		}
 		}
 		
+		// 결과 출력
 		System.out.println(sum);
 
 		sc.close();
